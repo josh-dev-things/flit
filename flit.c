45154e2bc148a07077ed1753d1dfc11a9c1317ba
Josh <ethankessler.dev@gmail.com>
2024-04-07 14:44:46 +0100

    inserting characters

--- flit.c
+++ flit.c
@@ -224,6 +224,28 @@ void editorAppendRow(char* s, size_t len) {
     E.numrows++;
 }
 
+void editorRowInsertChar(erow* row, int at, int c) {
+    if (at < 0 || at > row->size) {
+        at = row->size; // Interesting wraparound
+    }
+
+    row->chars = realloc(row->chars, row->size+2);
+    memmove(&row->chars[at+1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c) {
+    if(E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** file IO ***/
 
 void editorOpen(char* filename) {
@@ -459,6 +481,11 @@ void editorHandleKeyPress() {
         case RIGHT:
             editorMoveCursor(c);
             break;
+
+        default:
+            editorInsertChar(c);
+            break;
+        
     }
 }
 
