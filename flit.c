3f6cb299b9d300e22b11435ee9d4fac732b86f67
Josh <ethankessler.dev@gmail.com>
2024-04-09 18:57:17 +0100

    Newline

--- flit.c
+++ flit.c
@@ -217,10 +217,12 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
 }
 
-void editorAppendRow(char* s, size_t len) {
+void editorInsertRow(int at, char* s, size_t len) {
+    if(at < 0 || at > E.numrows) return;
+
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
     
-    int at = E.numrows;
     E.row[at].size = len;
     E.row[at].chars = malloc(len+1);
     memcpy(E.row[at].chars, s, len);
@@ -281,10 +283,25 @@ void editorRowDeleteChar(erow* row, int at) {
 
 void editorInsertChar(int c) {
     if(E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewline() {
+    if(E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow* row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx); // Split the current row in 2. Divide @ cusor position.
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDeleteChar() {
@@ -341,7 +358,7 @@ void editorOpen(char* filename) {
     while((linelen = getline(&line, &linecap, fp)) != -1) {
         while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
             linelen--;
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -558,7 +575,7 @@ void editorHandleKeyPress() {
 
     switch (c) {
         case '\r':
-            /* TODO */
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
