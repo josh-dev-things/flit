a890ddf802fffb7c00e0da7dc0c30f55fceebe70
josh-dev-things <ethankessler.dev@gmail.com>
2024-04-17 11:14:58 +0100

    Syntax highlighting for numbers & search

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-https://viewsourcecode.org/snaptoken/kilo/07.syntaxHighlighting.html
+https://viewsourcecode.org/snaptoken/kilo/07.syntaxHighlighting.html#restore-syntax-highlighting-after-search
 
 # Cool Title
 This bit was written using flit! (Thats pretty neat huh?)
--- flit.c
+++ flit.c
@@ -35,6 +35,12 @@ enum editorKey {
     P_DOWN
 };
 
+enum editorHighlight {
+    HL_NORMAL = 0,
+    HL_NUMBER,
+    HL_MATCH
+};
+
 /*** data ***/
 
 typedef struct erow {
@@ -42,6 +48,7 @@ typedef struct erow {
     int rsize;
     char* chars;
     char* render;
+    unsigned char* hl;
 } erow;
 
 struct editorConfig {
@@ -181,6 +188,28 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/** syntax highlighting ***/
+
+void editorUpdateSyntax(erow* row) {
+    row->hl = realloc(row->hl, row->rsize);
+    memset(row->hl, HL_NORMAL, row->rsize);
+
+    int i;
+    for(int i = 0; i < row->rsize; i++) {
+        if (isdigit(row->render[i])) {
+            row->hl[i] = HL_NUMBER;
+        }
+    }
+}
+
+int editorSyntaxToColor(int hl) {
+    switch (hl) {
+        case HL_NUMBER: return 31;
+        case HL_MATCH: return 34;
+        default: return 37;
+    }
+}
+
 /*** row operations ***/
 
 /// @brief Convert cx to rx
@@ -230,6 +259,8 @@ void editorUpdateRow(erow *row) {
     }
     row->render[idx] = '\0';
     row->rsize = idx;
+
+    editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char* s, size_t len) {
@@ -245,6 +276,7 @@ void editorInsertRow(int at, char* s, size_t len) {
 
     E.row[at].rsize = 0;
     E.row[at].render = NULL;
+    E.row[at].hl = NULL;
     editorUpdateRow(&E.row[at]);
 
     E.numrows++;
@@ -254,6 +286,7 @@ void editorInsertRow(int at, char* s, size_t len) {
 void editorFreeRow(erow* row) {
     free(row->render);
     free(row->chars);
+    free(row->hl);
 }
 
 void editorDelRow(int at) {
@@ -446,6 +479,8 @@ void editorFindCallback(char* query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
@@ -539,7 +574,31 @@ void editorDrawRows(struct abuf *ab) {
             int len = E.row[filerow].rsize - E.coloff;
             if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+            
+            char* c = &E.row[filerow].render[E.coloff];
+            unsigned char* hl = &E.row[filerow].hl[E.coloff];
+            int current_color = -1;
+
+            int j;
+            for(j = 0; j < len; j++) {
+                if (hl[j] == HL_NORMAL) {
+                    if(current_color != -1) {
+                        abAppend(ab, "\x1b[39m", 5);
+                        current_color = -1;
+                    }
+                    abAppend(ab, &c[j], 1);
+                } else {
+                    int color = editorSyntaxToColor(hl[j]);
+                    if (color != current_color) {
+                        current_color = color;
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+                        abAppend(ab, buf, clen);
+                    }
+                    abAppend(ab, &c[j], 1);
+                }
+            }
+            abAppend(ab, "\x1b[39m", 5);
         }
 
         abAppend(ab, "\x1b[K", 3); // Clearing screen by "Erasing in line"
